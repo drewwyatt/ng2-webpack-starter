2fea9c807af80cb76a732890872dce4e7469d212
Drew Wyatt <drew.j.wyatt@gmail.com>
2016-04-04 19:43:30 -0500

    added rxjs, application providers, and application directives to app bootstrap

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -2,4 +2,23 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
 
-bootstrap(AppComponent);
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES} from 'angular2/router';
+import {FORM_PROVIDERS} from 'angular2/common';
+
+import 'rxjs/Rx'
+
+const APPLICATION_PROVIDERS = [
+    HTTP_PROVIDERS,
+    ROUTER_PROVIDERS,
+    FORM_PROVIDERS
+];
+
+const APPLICATION_DIRECTIVES = [
+    ROUTER_DIRECTIVES
+];
+
+bootstrap(AppComponent, [
+    ...APPLICATION_PROVIDERS,
+    ...APPLICATION_DIRECTIVES
+]);
